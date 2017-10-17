c26b138b5a340e7d1d162f080c493b57dc68a73c
Djordje Madic <madicdjordje@gmail.com>
2017-10-17 10:27:34 +0200

    Hello world

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("madj");
+
+static int hello_init(void)
+{
+  pr_debug("Hello, world\n");
+  return 0;
+}
+
+static void hello_exit(void)
+{
+  pr_debug("Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
